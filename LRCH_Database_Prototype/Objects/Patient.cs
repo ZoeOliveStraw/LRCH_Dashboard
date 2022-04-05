593446ad22e7318dc05cf0f6437e4389065cd84a
ZoeOliveStraw <zoe.olive.straw@gmail.com>
2022-04-05 13:56:02 -0400

    Initial Commit

---
+++ LRCH_Database_Prototype/Objects/Patient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LRCH_Database_Prototype
+{
+    class Patient
+    {
+        public int patientNo { get; set; }
+        public string patientFirstName { get; set; }
+        public string patientLastName { get; set; }
+        public string patientAddress { get; set; }
+        public string patientCity { get; set; }
+        public string patientProvince { get; set; }
+        public string patientPostalCode { get; set; }
+    }
+}
