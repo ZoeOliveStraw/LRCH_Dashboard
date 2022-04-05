71ef95222f0eb73dc1b96fd19313746f1e4f4ca9
Kamrin Aubin <kamrinaubin@outlook.com>
2022-04-05 16:19:03 -0400

    Finish v1 ui design

--- LRCH_Database_Prototype/Form1.Designer.cs
+++ LRCH_Database_Prototype/Form1.Designer.cs
@@ -30,14 +30,40 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabRoomUtil = new System.Windows.Forms.TabPage();
+            this.labelWard4 = new System.Windows.Forms.Label();
+            this.labelWard3 = new System.Windows.Forms.Label();
+            this.labelIC = new System.Windows.Forms.Label();
+            this.labelSP = new System.Windows.Forms.Label();
+            this.labelP = new System.Windows.Forms.Label();
+            this.textBoxOccByBed = new System.Windows.Forms.TextBox();
+            this.textBoxOccByRoom = new System.Windows.Forms.TextBox();
+            this.labelOccByRoomType = new System.Windows.Forms.Label();
+            this.labelOccByBeds = new System.Windows.Forms.Label();
+            this.labelOccByRooms = new System.Windows.Forms.Label();
             this.tabPhysicianPatient = new System.Windows.Forms.TabPage();
             this.listBoxPatients = new System.Windows.Forms.ListBox();
             this.labelPatients = new System.Windows.Forms.Label();
             this.buttonSearch = new System.Windows.Forms.Button();
             this.textBoxPhysicianNo = new System.Windows.Forms.TextBox();
             this.labelPhysicianNo = new System.Windows.Forms.Label();
-            this.labelOccByRooms = new System.Windows.Forms.Label();
-            this.labelOccByBeds = new System.Windows.Forms.Label();
+            this.textBoxP = new System.Windows.Forms.TextBox();
+            this.textBoxSP = new System.Windows.Forms.TextBox();
+            this.textBoxIC = new System.Windows.Forms.TextBox();
+            this.textBoxW3 = new System.Windows.Forms.TextBox();
+            this.textBoxW4 = new System.Windows.Forms.TextBox();
+            this.labelBedsDischarging = new System.Windows.Forms.Label();
+            this.labelEmptyRooms = new System.Windows.Forms.Label();
+            this.textBoxBedDischarging = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabRoomUtil.SuspendLayout();
             this.tabPhysicianPatient.SuspendLayout();
@@ -55,6 +81,32 @@
             // 
             // tabRoomUtil
             // 
+            this.tabRoomUtil.Controls.Add(this.textBox1);
+            this.tabRoomUtil.Controls.Add(this.textBox2);
+            this.tabRoomUtil.Controls.Add(this.textBox3);
+            this.tabRoomUtil.Controls.Add(this.textBox4);
+            this.tabRoomUtil.Controls.Add(this.textBox5);
+            this.tabRoomUtil.Controls.Add(this.label1);
+            this.tabRoomUtil.Controls.Add(this.label2);
+            this.tabRoomUtil.Controls.Add(this.label3);
+            this.tabRoomUtil.Controls.Add(this.label4);
+            this.tabRoomUtil.Controls.Add(this.label5);
+            this.tabRoomUtil.Controls.Add(this.textBoxBedDischarging);
+            this.tabRoomUtil.Controls.Add(this.labelEmptyRooms);
+            this.tabRoomUtil.Controls.Add(this.labelBedsDischarging);
+            this.tabRoomUtil.Controls.Add(this.textBoxW4);
+            this.tabRoomUtil.Controls.Add(this.textBoxW3);
+            this.tabRoomUtil.Controls.Add(this.textBoxIC);
+            this.tabRoomUtil.Controls.Add(this.textBoxSP);
+            this.tabRoomUtil.Controls.Add(this.textBoxP);
+            this.tabRoomUtil.Controls.Add(this.labelWard4);
+            this.tabRoomUtil.Controls.Add(this.labelWard3);
+            this.tabRoomUtil.Controls.Add(this.labelIC);
+            this.tabRoomUtil.Controls.Add(this.labelSP);
+            this.tabRoomUtil.Controls.Add(this.labelP);
+            this.tabRoomUtil.Controls.Add(this.textBoxOccByBed);
+            this.tabRoomUtil.Controls.Add(this.textBoxOccByRoom);
+            this.tabRoomUtil.Controls.Add(this.labelOccByRoomType);
             this.tabRoomUtil.Controls.Add(this.labelOccByBeds);
             this.tabRoomUtil.Controls.Add(this.labelOccByRooms);
             this.tabRoomUtil.Location = new System.Drawing.Point(4, 22);
@@ -64,6 +116,94 @@
             this.tabRoomUtil.TabIndex = 0;
             this.tabRoomUtil.Text = "Room Utilization";
             this.tabRoomUtil.UseVisualStyleBackColor = true;
+            // 
+            // labelWard4
+            // 
+            this.labelWard4.AutoSize = true;
+            this.labelWard4.Location = new System.Drawing.Point(631, 161);
+            this.labelWard4.Name = "labelWard4";
+            this.labelWard4.Size = new System.Drawing.Size(45, 13);
+            this.labelWard4.TabIndex = 9;
+            this.labelWard4.Text = "Ward, 4";
+            // 
+            // labelWard3
+            // 
+            this.labelWard3.AutoSize = true;
+            this.labelWard3.Location = new System.Drawing.Point(529, 161);
+            this.labelWard3.Name = "labelWard3";
+            this.labelWard3.Size = new System.Drawing.Size(45, 13);
+            this.labelWard3.TabIndex = 8;
+            this.labelWard3.Text = "Ward, 3";
+            // 
+            // labelIC
+            // 
+            this.labelIC.AutoSize = true;
+            this.labelIC.Location = new System.Drawing.Point(397, 161);
+            this.labelIC.Name = "labelIC";
+            this.labelIC.Size = new System.Drawing.Size(75, 13);
+            this.labelIC.TabIndex = 7;
+            this.labelIC.Text = "Intensive Care";
+            // 
+            // labelSP
+            // 
+            this.labelSP.AutoSize = true;
+            this.labelSP.Location = new System.Drawing.Point(278, 161);
+            this.labelSP.Name = "labelSP";
+            this.labelSP.Size = new System.Drawing.Size(62, 13);
+            this.labelSP.TabIndex = 6;
+            this.labelSP.Text = "Semiprivate";
+            // 
+            // labelP
+            // 
+            this.labelP.AutoSize = true;
+            this.labelP.Location = new System.Drawing.Point(181, 161);
+            this.labelP.Name = "labelP";
+            this.labelP.Size = new System.Drawing.Size(40, 13);
+            this.labelP.TabIndex = 5;
+            this.labelP.Text = "Private";
+            // 
+            // textBoxOccByBed
+            // 
+            this.textBoxOccByBed.Location = new System.Drawing.Point(166, 46);
+            this.textBoxOccByBed.Name = "textBoxOccByBed";
+            this.textBoxOccByBed.ReadOnly = true;
+            this.textBoxOccByBed.Size = new System.Drawing.Size(100, 20);
+            this.textBoxOccByBed.TabIndex = 4;
+            // 
+            // textBoxOccByRoom
+            // 
+            this.textBoxOccByRoom.Location = new System.Drawing.Point(166, 9);
+            this.textBoxOccByRoom.Name = "textBoxOccByRoom";
+            this.textBoxOccByRoom.ReadOnly = true;
+            this.textBoxOccByRoom.Size = new System.Drawing.Size(100, 20);
+            this.textBoxOccByRoom.TabIndex = 3;
+            // 
+            // labelOccByRoomType
+            // 
+            this.labelOccByRoomType.AutoSize = true;
+            this.labelOccByRoomType.Location = new System.Drawing.Point(35, 161);
+            this.labelOccByRoomType.Name = "labelOccByRoomType";
+            this.labelOccByRoomType.Size = new System.Drawing.Size(140, 13);
+            this.labelOccByRoomType.TabIndex = 2;
+            this.labelOccByRoomType.Text = "Occupancy by Room Type: ";
+            // 
+            // labelOccByBeds
+            // 
+            this.labelOccByBeds.AutoSize = true;
+            this.labelOccByBeds.Location = new System.Drawing.Point(6, 49);
+            this.labelOccByBeds.Name = "labelOccByBeds";
+            this.labelOccByBeds.Size = new System.Drawing.Size(145, 13);
+            this.labelOccByBeds.TabIndex = 1;
+            this.labelOccByBeds.Text = "Overall Occupancy by Beds: ";
+            // 
+            // labelOccByRooms
+            // 
+            this.labelOccByRooms.AutoSize = true;
+            this.labelOccByRooms.Location = new System.Drawing.Point(6, 12);
+            this.labelOccByRooms.Name = "labelOccByRooms";
+            this.labelOccByRooms.Size = new System.Drawing.Size(154, 13);
+            this.labelOccByRooms.TabIndex = 0;
+            this.labelOccByRooms.Text = "Overall Occupancy by Rooms: ";
             // 
             // tabPhysicianPatient
             // 
@@ -122,23 +262,156 @@
             this.labelPhysicianNo.TabIndex = 5;
             this.labelPhysicianNo.Text = "Physician Number:";
             // 
-            // labelOccByRooms
+            // textBoxP
             // 
-            this.labelOccByRooms.AutoSize = true;
-            this.labelOccByRooms.Location = new System.Drawing.Point(6, 12);
-            this.labelOccByRooms.Name = "labelOccByRooms";
-            this.labelOccByRooms.Size = new System.Drawing.Size(154, 13);
-            this.labelOccByRooms.TabIndex = 0;
-            this.labelOccByRooms.Text = "Overall Occupancy by Rooms: ";
+            this.textBoxP.Location = new System.Drawing.Point(227, 158);
+            this.textBoxP.Name = "textBoxP";
+            this.textBoxP.ReadOnly = true;
+            this.textBoxP.Size = new System.Drawing.Size(45, 20);
+            this.textBoxP.TabIndex = 10;
             // 
-            // labelOccByBeds
+            // textBoxSP
             // 
-            this.labelOccByBeds.AutoSize = true;
-            this.labelOccByBeds.Location = new System.Drawing.Point(6, 37);
-            this.labelOccByBeds.Name = "labelOccByBeds";
-            this.labelOccByBeds.Size = new System.Drawing.Size(145, 13);
-            this.labelOccByBeds.TabIndex = 1;
-            this.labelOccByBeds.Text = "Overall Occupancy by Beds: ";
+            this.textBoxSP.Location = new System.Drawing.Point(346, 158);
+            this.textBoxSP.Name = "textBoxSP";
+            this.textBoxSP.ReadOnly = true;
+            this.textBoxSP.Size = new System.Drawing.Size(45, 20);
+            this.textBoxSP.TabIndex = 11;
+            // 
+            // textBoxIC
+            // 
+            this.textBoxIC.Location = new System.Drawing.Point(478, 158);
+            this.textBoxIC.Name = "textBoxIC";
+            this.textBoxIC.ReadOnly = true;
+            this.textBoxIC.Size = new System.Drawing.Size(45, 20);
+            this.textBoxIC.TabIndex = 12;
+            // 
+            // textBoxW3
+            // 
+            this.textBoxW3.Location = new System.Drawing.Point(580, 158);
+            this.textBoxW3.Name = "textBoxW3";
+            this.textBoxW3.ReadOnly = true;
+            this.textBoxW3.Size = new System.Drawing.Size(45, 20);
+            this.textBoxW3.TabIndex = 13;
+            // 
+            // textBoxW4
+            // 
+            this.textBoxW4.Location = new System.Drawing.Point(682, 158);
+            this.textBoxW4.Name = "textBoxW4";
+            this.textBoxW4.ReadOnly = true;
+            this.textBoxW4.Size = new System.Drawing.Size(45, 20);
+            this.textBoxW4.TabIndex = 14;
+            // 
+            // labelBedsDischarging
+            // 
+            this.labelBedsDischarging.AutoSize = true;
+            this.labelBedsDischarging.Location = new System.Drawing.Point(6, 83);
+            this.labelBedsDischarging.Name = "labelBedsDischarging";
+            this.labelBedsDischarging.Size = new System.Drawing.Size(148, 13);
+            this.labelBedsDischarging.TabIndex = 15;
+            this.labelBedsDischarging.Text = "# of Beds Discharging Today:";
+            // 
+            // labelEmptyRooms
+            // 
+            this.labelEmptyRooms.AutoSize = true;
+            this.labelEmptyRooms.Location = new System.Drawing.Point(6, 191);
+            this.labelEmptyRooms.Name = "labelEmptyRooms";
+            this.labelEmptyRooms.Size = new System.Drawing.Size(169, 13);
+            this.labelEmptyRooms.TabIndex = 16;
+            this.labelEmptyRooms.Text = "# of Empty Rooms by Room Type:";
+            // 
+            // textBoxBedDischarging
+            // 
+            this.textBoxBedDischarging.Location = new System.Drawing.Point(166, 80);
+            this.textBoxBedDischarging.Name = "textBoxBedDischarging";
+            this.textBoxBedDischarging.ReadOnly = true;
+            this.textBoxBedDischarging.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBedDischarging.TabIndex = 17;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(682, 188);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(45, 20);
+            this.textBox1.TabIndex = 27;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(580, 188);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(45, 20);
+            this.textBox2.TabIndex = 26;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(478, 188);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(45, 20);
+            this.textBox3.TabIndex = 25;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(346, 188);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(45, 20);
+            this.textBox4.TabIndex = 24;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(227, 188);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(45, 20);
+            this.textBox5.TabIndex = 23;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(631, 191);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Ward, 4";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(529, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Ward, 3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(397, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Intensive Care";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(278, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Semiprivate";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(181, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Private";
             // 
             // FormLRCHPrototype
             // 
@@ -170,6 +443,32 @@
         private System.Windows.Forms.Label labelPhysicianNo;
         private System.Windows.Forms.Label labelOccByBeds;
         private System.Windows.Forms.Label labelOccByRooms;
+        private System.Windows.Forms.Label labelP;
+        private System.Windows.Forms.TextBox textBoxOccByBed;
+        private System.Windows.Forms.TextBox textBoxOccByRoom;
+        private System.Windows.Forms.Label labelOccByRoomType;
+        private System.Windows.Forms.Label labelWard4;
+        private System.Windows.Forms.Label labelWard3;
+        private System.Windows.Forms.Label labelIC;
+        private System.Windows.Forms.Label labelSP;
+        private System.Windows.Forms.TextBox textBoxW4;
+        private System.Windows.Forms.TextBox textBoxW3;
+        private System.Windows.Forms.TextBox textBoxIC;
+        private System.Windows.Forms.TextBox textBoxSP;
+        private System.Windows.Forms.TextBox textBoxP;
+        private System.Windows.Forms.Label labelBedsDischarging;
+        private System.Windows.Forms.Label labelEmptyRooms;
+        private System.Windows.Forms.TextBox textBoxBedDischarging;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
